04bdd01a0153c215c3d3c5579986adcc97459b88
Amy Liu <mialana@users.noreply.github.com>
2025-09-14 16:12:04 -0400

    feat: simple scene stylization

---
+++ Assets/Scripts/Honey/HoneyChunk.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HoneyChunk : MonoBehaviour
+{
+    public float voxelSize = 0.5f;
+
+    public Vector3Int voxelsPerAxis;
+    public Bounds bounds;
+
+    private Collider[] colliders;
+
+    [HideInInspector]
+    public Mesh mesh;
+
+    MeshFilter meshFilter;
+    MeshRenderer meshRenderer;
+    MeshCollider meshCollider;
+
+    public UnityEngine.Vector4[] densityValues;
+
+    // Call after bounds are set
+    public void SetUp(Material mat)
+    {
+        // detect overlapping colliders
+        colliders = Physics.OverlapBox(center: bounds.center, halfExtents: bounds.extents);
+        if (colliders.Length != 0)
+        {
+            foreach (Collider c in colliders)
+                Debug.Log($"Found an object: {c.gameObject.name}");
+        }
+
+        UnityEngine.Vector3 size = bounds.size;
+        voxelsPerAxis[0] = Mathf.FloorToInt(size.x / voxelSize);
+        voxelsPerAxis[1] = Mathf.FloorToInt(size.y / voxelSize); // voxels are only above yCutoff
+        voxelsPerAxis[2] = Mathf.FloorToInt(size.z / voxelSize);
+
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshCollider = GetComponent<MeshCollider>();
+
+        if (meshFilter == null)
+        {
+            meshFilter = gameObject.AddComponent<MeshFilter>();
+        }
+
+        if (meshRenderer == null)
+        {
+            meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        }
+        if (meshCollider == null)
+        {
+            meshCollider = gameObject.AddComponent<MeshCollider>();
+        }
+
+        mesh = meshFilter.sharedMesh;
+        if (mesh == null)
+        {
+            mesh = new Mesh();
+            mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+            meshFilter.sharedMesh = mesh;
+        }
+
+        if (meshCollider.sharedMesh == null)
+        {
+            meshCollider.sharedMesh = mesh;
+        }
+        // force update
+        meshCollider.enabled = false;
+        meshCollider.enabled = true;
+
+        meshRenderer.material = mat;
+
+        // Initialize densityValues array based on the number of voxels
+        int numVoxels = voxelsPerAxis.x * voxelsPerAxis.y * voxelsPerAxis.z;
+        densityValues = new Vector4[numVoxels];
+    }
+
+    // Update is called once per frame
+    public void UpdateHoneyGrowth() { }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireCube(bounds.center, bounds.size);
+
+        if (densityValues == null || densityValues.Length == 0)
+        {
+            return;
+        }
+
+        // Draw spheres to debug density
+        // Gizmos.color = Color.white;
+
+        // for (int i = 0; i < densityValues.Length; i++)
+        // {
+        //     float density = densityValues[i].w;
+        //     if (density > 0)
+        //     {
+        //         Vector3 pos = new(densityValues[i].x, densityValues[i].y, densityValues[i].z);
+        //         Gizmos.DrawSphere(pos, density * 0.1f);
+        //     }
+        // }
+    }
+
+    public void OnApplicationQuit()
+    {
+        DestroyImmediate(gameObject, false);
+    }
+}
---
+++ Assets/Scripts/SkyboxManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SkyboxManager : MonoBehaviour
+{
+    public float skySpeed = 5f;
+    public Material skybox;
+
+    private float rotation = 0f;
+
+    void Awake()
+    {
+        if (!skybox)
+        {
+            skybox = Resources.Load("Materials/SkyboxMat", typeof(Material)) as Material;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rotation += Time.deltaTime * skySpeed;
+
+        if (rotation >= 360f)
+            rotation = 0f;
+
+        RenderSettings.skybox.SetFloat("_Rotation", rotation);
+    }
+}
